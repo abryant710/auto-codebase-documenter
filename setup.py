09cad909b98dc63f4f7ec6de2ecfc7466bc7d34a
Alex Bryant <alex.bryant@deeprecognition.io>
2023-05-22 12:16:20 +1000

    chore: bumping version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="auto-codebase-documenter",
-    version="0.3",
+    version="0.4",
     packages=find_packages(),
     install_requires=["openai", "python-dotenv", "PyYAML"],
     entry_points={
